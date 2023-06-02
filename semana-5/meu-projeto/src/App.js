34d883e6c5cfc0a2f38e7f58f773b3ab40e4399e
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-06-01 21:30:23 -0400

    finalizado a troca de linguagem

--- semana-5/meu-projeto/src/App.js
+++ semana-5/meu-projeto/src/App.js
@@ -4,27 +4,28 @@ import googleLogo from './img/googlelogo_color_272x92dp.png'
 // import Contador from './components/contador';
 import Btn from './components/Btn'
 import Search from './components/Search'
-import Language from './components/language'
+import Language from './components/language/Language'
+import { useState } from 'react'
 
 function App() {
-  const lang = "pt-br"
+  const [lang, setLang] = useState('br')
   
   return (
 		<div className="App">
-			<Header />
+			<Header setLang={setLang}/>
 			<img
 				className="logo"
 				src={googleLogo}
 				alt="imagem do logo do google"
 			/>
-			<Search />
+			<Search placeholder="O que você procura?"/>
 			<div className="btn-pesquisa">
 				<Btn
-					txt={lang === "pt-br" ? Language.BR.btn1 : Language.EN.btn1}
+					txt={lang === "br" ? Language.BR.btn1 : Language.EN.btn1}
 					prop2={25}
 				/>
 				<Btn
-					txt={lang === "pt-br" ? Language.BR.btn2 : Language.EN.btn2}
+					txt={lang === "br" ? Language.BR.btn2 : Language.EN.btn2}
 					prop2={30}
 				/>
 			</div>
--- semana-5/meu-projeto/src/components/header/Header.jsx
+++ semana-5/meu-projeto/src/components/header/Header.jsx
@@ -5,23 +5,33 @@ import AppImg from '../../img/apps_icon.svg'
 import BrFlag from '../../img/icons8-brasil-48.png'
 import UsaFlag from '../../img/icons8-eua-48.png'
 
-export default function Header() {
+export default function Header({setLang}) {
   return (
 		<div className="bg-blue flex">
 			<Btn txt="Gmail" />
 			<Btn txt="Drive" />
-			{/* <img className='flag' src={BrFlag} alt="Portugês" srcset="" />
-			<img className='flag' src={UsaFlag} alt="Inglês" srcset=""  /> */}
-      		<img 
+			<img 
+				className="flag" 
+				src={BrFlag} 
+				alt="Portugês" 
+				onClick={() => setLang('br')} 
+			/>
+			<img 
+				className="flag" 
+				src={UsaFlag} 
+				alt="Inglês" 
+				onClick={() => setLang('us')} 
+			/>
+			<img 
 				src={AppImg} 
-				alt="apps"
-				className='apps-icon'
+				alt="apps" 
+				className="apps-icon" 
 			/>
 			<img
 				src="https://img.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg?w=2000"
 				width={28}
 				alt="avatar"
-				className='avatar-img horizontal-shake'
+				className="avatar-img horizontal-shake"
 			/>
 		</div>
   );

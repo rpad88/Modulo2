2cf205d4fff43f4add6bd5a58976fe634e9b0979
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-06-07 21:40:54 -0400

    Header finalizado

---
+++ semana-6/my-store/src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+
+    const navList = [
+        {
+            label: 'Produtos',
+            route: '/produtos'
+        },
+        {
+            label: 'Contato',
+            route: '/contato'
+        }
+    ]
+
+    const link = navList.map(link => {
+        return (<li><a href={link.route}>{link.label}</a></li>)
+    })
+
+  return (
+    <header className='nav navbar bg-black justify-content-between'>
+        <div className="container align-items-center">
+            <h3>My Store</h3>   
+            <nav className='d-flex justify-content-around'>
+                {navList && link}
+            </nav>
+        </div>
+    </header>
+  )
+}

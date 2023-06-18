909850171d8fe29d96d18f393b02487cbdca4f12
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-06-18 19:32:51 -0400

    finalizado o exerício 2

--- Semana-7/pokedex/src/App.jsx
+++ Semana-7/pokedex/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+import { React, useState } from 'react'
 import './App.css'
 import Form from './assets/components/Form'
 import Header from './assets/components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [userList, setUserList] = useState([
+    {
+      nickname: "teste",
+      age: 23,
+      email: "teste@teste",
+      senha: "123"
+    },
+  ])
 
   return (
     <div className="app">
       <Header />
       <h1 className='text-center'>Hello World</h1>
-      <Form />
+      <Form userList={userList} setUserList={setUserList} />
+      <ul>
+        {
+          userList.map((user) =>{
+            return <li key={user.nickname}>{user.nickname}</li>
+          })
+        }
+      </ul>
     </div>
   )
 }
--- Semana-7/pokedex/src/assets/components/Form/Form.jsx
+++ Semana-7/pokedex/src/assets/components/Form/Form.jsx
@@ -1,48 +1,63 @@
 import React from 'react'
 import Input from './Input'
 
-function printInputValues() {
-	// e.preventDefault()
-  const nickname = document.myForm.nickname
-  const age = document.myForm.age
-  const email = document.myForm.email
-  const senha = document.myForm.senha
+export default function Form({userList, setUserList}) {
+  // function printInputValues() {
+  //   const nickname = document.myForm.nickname
+  //   const age = document.myForm.age
+  //   const email = document.myForm.email
+  //   const senha = document.myForm.senha
+  
+  //   console.log("nickname:", nickname.value)
+  //   console.log("age:", age.value)
+  //   console.log("email:", email.value)
+  //   console.log("senha:", senha.value)
+  // }
+  
+  function formValidation() {
+    console.log('formValidation()')
+    const nickname = document.myForm.nickname
+    const age = document.myForm.age
+    const email = document.myForm.email
+    const senha = document.myForm.senha
+  
+    if(!nickname.value) return setErrorFor(nickname, "Nickname cannot be empty")
+    if(!age.value) return setErrorFor(age, "age cannot be empty")
+    if(!email.value) return setErrorFor(email, "email cannot be empty")
+    if(!senha.value) return setErrorFor(senha, "senha cannot be empty")
+  
+    // printInputValues()
+    console.log(nickname.value, age.value, email.value, senha.value)
+    return {
+            nickname: nickname.value,
+            age: age.value,
+            email: email.value,
+            senha: senha.value
+          }
+  }
+  
+  function setErrorFor(input, errorMsg) {
+    const fieldset = input.parentElement
+    const small = fieldset.querySelector('small')
+  
+    small.removeAttribute('class')
+    small.innerText = errorMsg
+  }
 
-	console.log("nickname:", nickname.value)
-	console.log("age:", age.value)
-	console.log("email:", email.value)
-	console.log("senha:", senha.value)
-}
-
-function formValidation(e) {
-  e.preventDefault()
-  console.log('formValidation()')
-  const nickname = document.myForm.nickname
-  const age = document.myForm.age
-  const email = document.myForm.email
-  const senha = document.myForm.senha
-
-  if(!nickname.value) return setErrorFor(nickname, "Nickname cannot be empty")
-  if(!age.value) return setErrorFor(age, "age cannot be empty")
-  if(!email.value) return setErrorFor(email, "email cannot be empty")
-  if(!senha.value) return setErrorFor(senha, "senha cannot be empty")
-
-  printInputValues()
-
-}
-
-function setErrorFor(input, errorMsg) {
-  const fieldset = input.parentElement
-  const small = fieldset.querySelector('small')
-
-  small.removeAttribute('class')
-  small.innerText = errorMsg
-}
-
-export default function Form() {
+  function register(e) {
+    e.preventDefault()
+    try {
+      const {nickname, age, email, senha} = formValidation()
+      const newUser = {nickname, age, email, senha}
+      console.log(newUser)
+      setUserList([...userList, newUser])
+    } catch (error) {
+      console.log(error.message)      
+    }
+  }
 
   return (
-    <form name='myForm' onSubmit={formValidation} className='container'>
+    <form name='myForm' onSubmit={register} className='container'>
         <Input fieldName="nickname" placeholder="Insert your Nickname" />
         <Input fieldName="age" type="number" placeholder="Insert your age" />
         <Input fieldName="email" type="email" placeholder="Insert your e-mail" />
